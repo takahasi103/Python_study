ee157dc1847af501fae15969ec79cf229a1f72a4
takahasi <124013519+takahasi103@users.noreply.github.com>
2023-07-26 16:41:29 +0900

    [Add] edit_food

--- Desktop/Python_study/order/food.py
+++ Desktop/Python_study/order/food.py
@@ -43,3 +43,33 @@ class Food(MenuItem):
             foods['food' + str(i)] = Food(name, price, calorie)
             i += 1
         return foods
+    
+    def edit_food(foods):
+        Food.print_menu(foods)
+        i = int(input('どのfoodメニューを編集しますか？'))
+        food_key = 'food' + str(i)
+        if food_key in foods:
+            selected_food = foods[food_key]
+            print("選択された食べ物メニューの情報：")
+            print('1.名前：' + selected_food.name)
+            print('2.値段：¥' + str(selected_food.price))
+            print('3.カロリー：' + str(selected_food.calorie) + 'kcal')
+            n = int(input('どの値を編集しますか？（1:名前 / 2:値段 / 3:カロリー）'))
+            if n == 1:
+                name = str(input('新しい名前：'))
+                selected_food.name = name
+            elif n == 2:
+                price = int(input('新しい値段：'))
+                selected_food.price = price
+            elif n == 3:
+                calorie = int(input('新しいカロリー：'))
+                selected_food.calorie = calorie
+            else:
+                print("無効な選択です。")
+            
+            print("編集後の食べ物メニューの情報：")
+            print('名前：' + selected_food.name)
+            print('値段：¥' + str(selected_food.price))
+            print('カロリー：' + str(selected_food.calorie) + 'kcal')
+        else:
+            print("指定された番号の食べ物メニューは存在しません。")
